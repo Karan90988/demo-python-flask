0d1ac0b4e6b7df75e625a094843925fa14401cf4
Karan <choudharykaran909888@gmail.com>
2025-01-20 17:18:43 +0530

    new commit

--- stop_container.sh
+++ stop_container.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
-#set -e
-
+set -e
+echo "hello"
 # Stop the running container (if any)
 #sudo su - root
 #sudo docker stop $(ps -a -q)
 #sudo docker rm $(ps -a -q)
---------
-#!/bin/bash
-set -e
-
-# Retrieve the IDs of all containers (both running and stopped)
-ALL_CONTAINERS=$(sudo docker ps -a -q)
-
-if [ -n "$ALL_CONTAINERS" ]; then
-    echo "Stopping and removing containers: $ALL_CONTAINERS"
-    # Stop all running containers
-    sudo docker stop $ALL_CONTAINERS || true
-    # Remove all containers
-    sudo docker rm $ALL_CONTAINERS || true
-else
-    echo "No containers to stop or remove."
-fi
-
 

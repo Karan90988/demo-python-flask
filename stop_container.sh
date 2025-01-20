e77ceb399967cd0f08fcf97b07a01bbe9e42a928
Karan <choudharykaran909888@gmail.com>
2025-01-20 17:46:04 +0530

    new commit

--- stop_container.sh
+++ stop_container.sh
@@ -2,7 +2,10 @@
 set -e
 echo "hello"
 # Stop the running container (if any)
-#sudo su - root
-#sudo docker stop $(ps -a -q)
+sudo su - root
+#sudo docker stop ps -a -q
 #sudo docker rm $(ps -a -q)
+sudo docker stop $(sudo docker ps -a -q)  # Stop all running containers
+sudo docker rm $(sudo docker ps -a -q)    # Remove all containers
+
 

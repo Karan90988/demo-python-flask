11db279dae9368909540a72c3608ce9f1c17866a
Karan <choudharykaran909888@gmail.com>
2025-01-20 16:25:45 +0530

    new commit

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
-
+sudo su - root
 # Pull the Docker image from Docker Hub
-echo
-
+docker pull karan90988/demo-python-flask:latest
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 karan90988/demo-python-flask:latest
--- stop_container.sh
+++ stop_container.sh
@@ -3,3 +3,6 @@ set -e
 
 # Stop the running container (if any)
 echo "Hi"
+#sudo su - root
+#docker stop $(ps -a -q)
+#docker rm $(ps -a -q)
